c6fefb13948ab6f17c04648a9afe1f06d4d6ae05
yetimenz <enesyetim@hotmail.com>
2023-08-17 23:14:12 +0200

    counting.project

---
+++ app.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('btn');
+
+button.addEventListener('click', function(){
+
+    let word = document.getElementById('str').value;
+    let count = word.length;
+    let outputDiv = document.getElementById('output');
+
+    outputDiv.innerHTML = `<h1>${count}</h1>`
+
+
+
+
+});
